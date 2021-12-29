4694f0b20eb9287aa1f7fa7a4bc6c007d41aa67a
Si7A-00 <Si7A-00@Si7C-02>
2021-12-29 12:32:03 +0900

    1st commit

---
+++ Proj1229/src/Jungol516.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Jungol516 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		double a = sc.nextDouble();
+		double b = sc.nextDouble();
+		String c = sc.next();
+		sc.close();
+		
+		
+		System.out.printf("%.2f\n", a);
+		System.out.printf("%.2f\n", b);
+		System.out.println(c);
+		
+		
+	}
+
+}
